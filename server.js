a1023dce6150160422952bf3a22e866ff1680df5
Josue Hernandez <ealvarezjh@gmail.com>
2020-05-08 17:57:27 -0600

    Configuraciones para subir a Heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 var hbs = require('hbs');
 
 const app = express()
-const port = 3000;
+const port = process.env.port || 3000;
 require('./hbs/helpers');
 
 app.use(express.static(__dirname + '/public'));
